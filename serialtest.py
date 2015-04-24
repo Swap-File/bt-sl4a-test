ab50a33237e3603b459be8cf5c28fc07aadf5871
swap-file <bigmarkslp@gmail.com>
2015-04-24 10:17:48 -0500

    Add benchmarks

--- serialtest.py
+++ serialtest.py
@@ -1,8 +1,5 @@
 #qpy:console
 
-#Example Bluetooth Serial Script for QPython3 on Android 5.0
-#connID is NOT optional
-
 #this is the SPP UUID (Serial Port Profile)
 ssp_uuid = '00001101-0000-1000-8000-00805F9B34FB'
 #this is my bluetooth devices ID
@@ -44,31 +41,63 @@ while True:
 		droid.bluetoothConnect(ssp_uuid, BT_DEVICE_ID)
 		time.sleep(1)
 		if len(droid.bluetoothActiveConnections().result) != 0:
-			#get the connID - being lazy and using the first connID in the list
-			#if you have more than one bluetooth connection parse it out and find yours
+			#get the connIDd - being lazy and using the first (and only) connID in the list
+			#if I had more than one bluetooth connection at a time I would need to find my connID
+			#conID is NOT OPTIONAL and MUST Be used when reading data
 			connID = list(droid.bluetoothActiveConnections().result.keys())[0]
+			
 			print ("Connected")
 			print ("connID: " + connID)
 
 	#this works for the range of 7 bit ASCII (0-127)
-	#above that, correct character will not be sent
-	for i in range(0, 128):
-		droid.bluetoothWrite(chr(i),connID)
+	#above that, android won't send the right characters
+	print ("Sending 256 characters one at a time")
+	start_time = time.time()
+	for j in range(0, 2):	
+		for i in range(0, 128):
+			droid.bluetoothWrite(chr(i),connID)
+	print("--- %s seconds ---" % (time.time() - start_time))
+	#about 0.58 seconds on my S4
 	
-	#the encoding here is a mess, but it works for sending 8-bit values
+	print ("Sending 256 characters as a list")
+	start_time = time.time()
+	data = []
+	for j in range(0, 2):	
+		for i in range(0, 128):
+			data.append(chr(i))
+	droid.bluetoothWrite(data,connID)
+	print("--- %s seconds ---" % (time.time() - start_time))
+	#about 0.0044 seconds on my S4
+	
+	#the data encoding here is a mess, but it works to send 8-bit values
 	#b64encode changed what it requires for input and output between python 2 and 3
+	print ("Sending 256 bytes one at a time")
+	start_time = time.time()
 	for i in range(0, 256):
-		droid.bluetoothWriteBinary(b64encode(chr(i).encode('latin1')).decode(),connID)
+		droid.bluetoothWriteBinary(b64encode(bytearray([i])).decode(),connID)
+	print("--- %s seconds ---" % (time.time() - start_time))
+	#about 0.57 seconds on my S4
 	
 
+	#the data encoding here is a mess, but it works to send 8-bit values
+	#b64encode changed what it requires for input and output between python 2 and 3
+	print ("Sending 256 bytes as a byte array")
+	start_time = time.time()
+	data = bytearray()
+	for i in range(0, 256):
+		data.append(i)
+	droid.bluetoothWriteBinary(b64encode(data).decode(),connID)
+	print("--- %s seconds ---" % (time.time() - start_time))
+	#about 0.0025 seconds on my S4
+	
 	while True:
-		#this works for reading the range of 8-bit values (0-255)
+		#this works for the range of 8-bit values (0-255)
 		#requires extra encode due to changes in python 3
 		if(droid.bluetoothReadReady(connID).result == True):		
 			print (ord(b64decode(droid.bluetoothReadBinary(1,connID).result.encode())))
 			
 	while True:
-		#this works for reading the range of 7 bit ASCII (0-127)
+		#this works for the range of 7 bit ASCII (0-127)
 		#above that, android won't read the right characters
 		if(droid.bluetoothReadReady(connID).result == True):
 			print (ord((droid.bluetoothRead(1,connID).result)))
