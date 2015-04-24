c1a0fefc67899cef679620f87fe9fc3480b7e992
swap-file <bigmarkslp@gmail.com>
2015-04-24 12:41:44 -0500

    Add Reading Benchmarks

--- serialtest.py
+++ serialtest.py
@@ -90,16 +90,83 @@ while True:
 	print("--- %s seconds ---" % (time.time() - start_time))
 	#about 0.0025 seconds on my S4
 	
+	#receive benchmarks require your arduino to send 256 bytes...
+	
+
+	
 	while True:
-		#this works for the range of 8-bit values (0-255)
-		#requires extra encode due to changes in python 3
-		if(droid.bluetoothReadReady(connID).result == True):		
-			print (ord(b64decode(droid.bluetoothReadBinary(1,connID).result.encode())))
-			
-	while True:
+		bytes_rec = [];
+		while (len(bytes_rec) < 256):
+			#this works for the range of 8-bit values (0-255)
+			#requires extra encode due to changes in python 3
+			if(droid.bluetoothReadReady(connID).result == True):
+				if (len(bytes_rec) == 0):
+					print ("Getting 256 bytes one at a time")
+					start_time = time.time()
+				bytes_rec.append(ord(b64decode(droid.bluetoothReadBinary(1,connID).result.encode())))
+		print("--- %s seconds ---" % (time.time() - start_time))
+		#about 1.1 seconds on my S4
+		#pprint.pprint(bytes_rec)
+		
+		bytes_rec = [];
+		while (len(bytes_rec) < 256):
 		#this works for the range of 7 bit ASCII (0-127)
 		#above that, android won't read the right characters
-		if(droid.bluetoothReadReady(connID).result == True):
-			print (ord((droid.bluetoothRead(1,connID).result)))
-			
-			
+			if(droid.bluetoothReadReady(connID).result == True):
+				if (len(bytes_rec) == 0):
+					print ("Getting 256 characters one at a time")
+					start_time = time.time()
+				bytes_rec.append(ord((droid.bluetoothRead(1,connID).result)))
+		print("--- %s seconds ---" % (time.time() - start_time))
+		#about 1.2 seconds on my S4
+		#pprint.pprint(bytes_rec)	
+
+		
+		#this works for the range of 8-bit values (0-255)
+		#requires extra encode due to changes in python 3
+		bytes_rec = bytearray();
+		reads_taken = 0;
+		while (len(bytes_rec) < 256):
+			#put code here to break out of the while loop if your bt device goes silent
+			#either watch for a number of bytes read, or a end of line character
+			#break if too much time passes
+			if(droid.bluetoothReadReady(connID).result == True):
+				reads_taken = reads_taken + 1
+				if (len(bytes_rec) == 0):
+					print ("Trying to get 256 bytes all at once")
+					start_time = time.time()
+				#only read how many bytes we need (256-len(bytes_rec))
+				bytes_rec.extend(b64decode(droid.bluetoothReadBinary(256-len(bytes_rec),connID).result.encode()))
+		output = []
+		for item in bytes_rec:
+			output.append(int(item))
+		print("--- %s seconds ---" % (time.time() - start_time))
+		print("Took this many Reads: " + str(reads_taken))
+		#pprint.pprint(output)
+		#about 0.01 to 0.05 seconds on my S4
+		#time depends mostly on how many separate reads it takes
+		#generally between 2 to 4 separate reads to finish	
+		
+		bytes_rec = ''
+		reads_taken = 0;
+		while (len(bytes_rec) < 256):
+			#put code here to break out of the while loop if your bt device goes silent
+			#either watch for a number of bytes read, or a end of line character
+			#break if too much time passes
+			if(droid.bluetoothReadReady(connID).result == True):
+				reads_taken = reads_taken + 1
+				if (len(bytes_rec) == 0):
+					print ("Trying to get 256 characters all at once")
+					start_time = time.time()
+				#only read how many bytes we need (256-len(bytes_rec))
+				bytes_rec = bytes_rec + droid.bluetoothRead(256-len(bytes_rec),connID).result
+		output = []
+		for item in bytes_rec:
+			output.append(ord(item))
+		print("--- %s seconds ---" % (time.time() - start_time))
+		print("Took this many Reads: " + str(reads_taken))
+		#pprint.pprint(output)
+		#about 0.01 to 0.05 seconds on my S4
+		#time depends mostly on how many separate reads it takes
+		#generally between 2 to 4 separate reads to finish	
+	
